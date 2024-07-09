fa1dfb6dd9d2ed503af30c29c4908c687a448323
shivani <shivani@vega6.com>
2024-07-09 18:19:36 +0530

    update

--- portfolio/src/components/Contact/Contact.jsx
+++ portfolio/src/components/Contact/Contact.jsx
@@ -1,35 +1,38 @@
 import React from 'react'
 
 const Contact = () => {
+    const handleSubmit = () => {
+
+    }
     return (
         <>
             <div className=' flex flex-col justify-center items-center'>
                 <div className='w-3/5'>
                     <h2 className='text-5xl font-semibold text-center '>Any Question? Feel Free to Contact</h2>
 
-                    <form className=' py-5'>
+                    <form className=' py-5' onSubmit={handleSubmit}>
                         <div className='flex justify-center items-center gap-6'>
                             <div className="mb-3 w-full">
                                 <label className="form-label text-lg font-semibold ">Name</label>
-                                <input type="text" className="form-control h-12 placeholder-gray-400 ::placeholder" placeholder="Enter the name" />
+                                <input type="text" className="form-control h-12 placeholder-gray-400 ::placeholder" placeholder="Enter the name" required />
                             </div>
                             <div className="mb-3 w-full ">
                                 <label className="form-label text-lg font-semibold">Email</label>
-                                <input type="email" className="form-control h-12 placeholder-gray-400 ::placeholder" placeholder="Enter the email" />
+                                <input type="email" className="form-control h-12 placeholder-gray-400 ::placeholder" placeholder="Enter the email" required />
                             </div>
                         </div>
                         <div className="mb-3">
                             <label className="form-label text-lg font-semibold">Phone</label>
-                            <input type="email" className="form-control h-12 placeholder-gray-400 ::placeholder" placeholder="Enter the phone number" />
+                            <input type="email" className="form-control h-12 placeholder-gray-400 ::placeholder" placeholder="Enter the phone number" required />
                         </div>
                         <div className="mb-3">
                             <label className="form-label text-lg font-semibold">Subject</label>
-                            <input type="email" className="form-control h-12 placeholder-gray-400 ::placeholder" placeholder="Enter Subject" />
+                            <input type="email" className="form-control h-12 placeholder-gray-400 ::placeholder" placeholder="Enter Subject" required />
                         </div>
 
                         <div className="mb-3">
                             <label className="form-label text-lg font-semibold">Your Message</label>
-                            <textarea className="form-control placeholder-gray-400 ::placeholder" rows="4"></textarea>
+                            <textarea className="form-control placeholder-gray-400 ::placeholder" rows="4" required></textarea>
                         </div>
                         <button className='border-2 border-slate-900 text-lg px-2 py-1 rounded-lg'>Submit</button>
                     </form>
--- portfolio/src/components/Navbar/Navbar.jsx
+++ portfolio/src/components/Navbar/Navbar.jsx
@@ -1,18 +1,27 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Navbar = () => {
     const [toggler, setToggler] = useState(false)
     const handleToggle = () => {
         setToggler(!toggler)
+        localStorage.setItem("themeMode", !toggler)
     }
     console.log(toggler, "toggler")
+
+    useEffect(() => {
+        let theme = localStorage.getItem("themeMode")
+        if (theme) {
+            setToggler(theme)
+
+        }
+    }, [])
     return (
-        <div>
-            <nav className="bg-inherit  py-5">
+        <>
+            <nav className={`bg-inherit ${toggler ? 'bg-dark' : 'bg-light'} ${toggler ? 'text-white' : 'text-black'}`}>
                 <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 flex justify-between items-center ">
                     <div className="relative flex h-16 items-center justify-between">
                         <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                            <button type="button" className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+                            <button type="button" className={`${toggler ? ' hover:bg-gray-100' : ' hover:bg-gray-700'}relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false"`}>
                                 <span className="absolute -inset-0.5"></span>
                                 <span className="sr-only">Open main menu</span>
 
@@ -29,24 +38,25 @@ const Navbar = () => {
 
                             <div className="hidden sm:ml-6 sm:block">
                                 <div className="flex space-x-4">
-                                    <a href="/" className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Home</a>
-                                    <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-black-300 hover:bg-gray-700 hover:text-white">About</a>
-                                    <a href="#" className="rounded-md    px-3 py-2 text-sm font-medium text-black-300 hover:bg-gray-700 hover:text-white">Portfolio</a>
-                                    <a href="/contact" className="rounded-md px-3 py-2 text-sm font-medium text-black-300 hover:bg-gray-700 hover:text-white">Contact</a>
+                                    <a href="/" className={`rounded-md px-3 py-2 text-sm font-medium ${toggler ? 'text-white' : 'text-black-300'} ${toggler ? 'hover:bg-gray-700' : 'hover:bg-gray-100'} hover:text-white`}>Home</a>
+                                    <a href="/about" className={`rounded-md px-3 py-2 text-sm font-medium ${toggler ? 'text-white' : 'text-black-300'} ${toggler ? 'hover:bg-gray-700' : 'hover:bg-gray-100'} hover:text-white`}>About</a>
+                                    <a href="/portfolio" className={`rounded-md px-3 py-2 text-sm font-medium ${toggler ? 'text-white' : 'text-black-300'} ${toggler ? 'hover:bg-gray-700' : 'hover:bg-gray-100'} hover:text-white`}>Portfolio</a>
+                                    <a href="/contact" className={`rounded-md px-3 py-2 text-sm font-medium ${toggler ? 'text-white' : 'text-black-300'} ${toggler ? 'hover:bg-gray-700' : 'hover:bg-gray-100'} hover:text-white`}>Contact</a>
+
                                 </div>
                             </div>
                         </div>
 
                     </div>
                     <div class="form-check form-switch">
-                        <input class="form-check-input" type="checkbox" onClick={handleToggle} value={toggler} role="switch" id="flexSwitchCheckDefault" />
+                        <input class="form-check-input" type="checkbox" onClick={handleToggle} checked={toggler} role="switch" id="flexSwitchCheckDefault" />
                         <label class="form-check-label" for="flexSwitchCheckDefault">Dark Mode</label>
                     </div>
                 </div>
 
                 <div className="sm:hidden" id="mobile-menu">
                     <div className="space-y-1 px-2 pb-3 pt-2">
-                        <a href="#" className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white" aria-current="page">Home</a>
+                        <a href="/" className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white" aria-current="page">Home</a>
                         <a href="#" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
                         <a href="#" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Portfolio</a>
                         <a href="#" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
@@ -54,8 +64,7 @@ const Navbar = () => {
                 </div>
             </nav>
 
-
-        </div>
+        </>
     )
 }
 
